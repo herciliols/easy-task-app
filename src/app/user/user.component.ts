fff53f0f8e7b9d8ad57e7ad8c3912fe55356045a
herciliols <hercilioluzs@gmail.com>
2024-06-03 12:29:02 +0100

    refactor: acionar classe CSS em cima do separador ao passar o mouse sobre ele

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { User } from './user.model' 
@@ -17,10 +17,8 @@ export class UserComponent {
   // @Input({required: true}) id!: string;
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
-     @Input({required: true}) user!: User;
-
-
-
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
   //select = output<string>();
 
