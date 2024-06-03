778b18b18cfda3ef27d07530d83cacc5d342943f
herciliols <hercilioluzs@gmail.com>
2024-06-03 14:12:07 +0100

    feat: adiciona funcionalidade de abrir e fechar uma nova tarefa com o estilo CSS;

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isAddingTask: boolean = false;
   tasks = [
     {
         "id": "t1",
@@ -44,4 +46,12 @@ export class TasksComponent {
   onCompleteTask(id: string){
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }  
 }
